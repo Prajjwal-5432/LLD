e23b193a22896d0d326b3f9c89ca2108b6d3f203
prajjwal <prajjwalsahu5432@gmail.com>
2023-07-04 00:10:27 +0530

    Builder Design Pattern

---
+++ src/BuilderFactory/Client.java
@@ -0,0 +1,29 @@
+package BuilderFactory;
+
+public class Client {
+    public static void main(String[] args) {
+        UserExam userExam;
+
+        try {
+//            instead of keeping the setters void use method chaining
+//            UserExam.UserExamBuilder userExamBuilder = UserExam.getUserExamBuilder();
+//            userExamBuilder.setName("Prajjwal");
+//            userExamBuilder.setEnglishMarks(80);
+//            userExamBuilder.setMathMarks(90);
+//            userExamBuilder.setEnglishMarks(78);
+//            userExamBuilder.setScienceMarks(89);
+//
+//            userExam = userExamBuilder.build();
+
+            userExam = UserExam.getUserExamBuilder()
+                                .setName("Prajjwal")
+                                .setEnglishMarks(90)
+                                .setScienceMarks(92)
+                                .setMathMarks(89)
+                                .build();
+
+        } catch (Exception exception) {
+            System.out.println("Some parameters are wrong");
+        }
+    }
+}
---
+++ src/BuilderFactory/UserExam.java
@@ -0,0 +1,57 @@
+package BuilderFactory;
+
+import java.security.InvalidParameterException;
+
+public class UserExam {
+    private int englishMarks;
+    private int mathMarks;
+    private int scienceMarks;
+    private String Name;
+    private UserExam() {}
+
+    public static UserExamBuilder getUserExamBuilder() {
+        return new UserExamBuilder();
+    }
+    public static class UserExamBuilder {
+        private int englishMarks;
+        private int mathMarks;
+        private int scienceMarks;
+        private String Name;
+
+        public UserExamBuilder setEnglishMarks(int englishMarks) {
+            this.englishMarks = englishMarks;
+            return this;
+        }
+
+        public UserExamBuilder setMathMarks(int mathMarks) {
+            this.mathMarks = mathMarks;
+            return this;
+        }
+
+        public UserExamBuilder setScienceMarks(int scienceMarks) {
+            this.scienceMarks = scienceMarks;
+            return this;
+        }
+
+        public UserExamBuilder setName(String name) {
+            Name = name;
+            return this;
+        }
+
+        public UserExam build() {
+            //Step1: validate the attributes
+            if(englishMarks > 100 || mathMarks > 100 || scienceMarks > 100) {
+                throw new InvalidParameterException("Marks cannot be greater than 100");
+            }
+
+            //Step2: create the parent
+            UserExam userExam = new UserExam();
+            userExam.englishMarks = this.englishMarks;
+            userExam.mathMarks = this.mathMarks;
+            userExam.scienceMarks = this.scienceMarks;
+            userExam.Name = this.Name;
+
+            return userExam;
+        }
+    }
+}
